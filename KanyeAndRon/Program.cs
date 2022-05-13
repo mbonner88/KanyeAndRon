ef25eb9d7775a663d7a90e3bad3180da0669e9d1
Matthew Bonner <matthewsbonner@comcast.net>
2022-05-13 12:07:26 -0400

    init commit

---
+++ KanyeAndRon/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeAndRon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"");
+                Console.WriteLine("Kanye West: " + "\"" + KanyeQuote() + "\"");
+                Console.WriteLine("Ron Swanson: " + RonQuote());
+            }
+        }
+
+        static string KanyeQuote()
+        {
+            var client = new HttpClient();
+            var kanyeResponse = client.GetStringAsync("https://api.kanye.rest").Result;
+            return JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+        }
+
+        static string RonQuote()
+        {
+            var client = new HttpClient();
+            var ronResponse = client.GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes").Result;
+            return JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+        }
+    }
+}
